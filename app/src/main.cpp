7edf7f72c868d4da7389f5d3d1202f21e5c93464
Carlos Perez <perez@doorstep.com>
2026-03-28 12:40:05 -0600

    Use TRACE_ macros to log to remoteproc trace0

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,15 +1,24 @@
+#include <stdio.h>
+
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
-#include <zephyr/logging/log.h>
 
 #define SLEEP_TIME_MS 1000
+
+/*
+ * Zephyr's LOG_INF backend doesn't output to ram_console/trace0.
+ * Only printf() reaches /sys/kernel/debug/remoteproc/remoteproc0/trace0.
+ * Use these macros to get LOG_INF-style output through printf.
+ */
+#define TRACE_ERR(fmt, ...) printf("[ERR] " fmt "\n", ##__VA_ARGS__)
+#define TRACE_WRN(fmt, ...) printf("[WRN] " fmt "\n", ##__VA_ARGS__)
+#define TRACE_INF(fmt, ...) printf("[INF] " fmt "\n", ##__VA_ARGS__)
+#define TRACE_DBG(fmt, ...) printf("[DBG] " fmt "\n", ##__VA_ARGS__)
 
 /* The devicetree node identifier for the "led0" alias. */
 #define LED_NODE DT_ALIAS(led0)
 
 static const struct gpio_dt_spec led = GPIO_DT_SPEC_GET(LED_NODE, gpios);
-
-LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
 
 int main(void)
 {
@@ -19,11 +28,13 @@ int main(void)
 
     if (gpio_pin_configure_dt(&led, GPIO_OUTPUT_ACTIVE) < 0) return 0;
 
+    TRACE_INF("LED application started - trace0 enabled");
+
     while (1) {
         if (gpio_pin_toggle_dt(&led) < 0) return 0;
 
         led_state = !led_state;
-        printf("LED state: %s\n", led_state ? "ON" : "OFF");
+        TRACE_INF("LED state: %s", led_state ? "ON" : "OFF");
         k_msleep(SLEEP_TIME_MS);
     }
     return 0;
