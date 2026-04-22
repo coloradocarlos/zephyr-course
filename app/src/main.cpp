56a0cbca7a1cd3019c5bd80a01be1f409331d566
Carlos Perez <perez@doorstep.com>
2026-04-21 20:49:31 -0600

    Add APP_HEARTBEAT_PERIOD_MS (#3)
    * Add APP_HEARTBEAT_PERIOD_MS
    
    * Add DT comments and clean-up main.cpp defines

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
+#include <zephyr/autoconf.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
-
-#define SLEEP_TIME_MS 1000
 
 /*
  * Zephyr's LOG_INF backend doesn't output to ram_console/trace0.
@@ -17,15 +16,15 @@
 #define TRACE_DBG(fmt, ...) printf("[DBG] " fmt "\n", ##__VA_ARGS__)
 
 /* The devicetree node identifier for the "led0" and "led1" aliases. */
-#define LED_NODE DT_ALIAS(led0)
-#define LED_NODE_2 DT_ALIAS(led1)
+#define LED_NODE_0 DT_ALIAS(led0)
+#define LED_NODE_1 DT_ALIAS(led1)
 
 // The OSD32MP1 Breakout Board has two LEDs: Green and Red.
 // The Green LED is connected to the GPIOI pin 9 and the Red LED is connected to the GPIOI pin 8.
 // Green LED configuration:
-static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(LED_NODE, gpios);
+static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(LED_NODE_0, gpios);
 // Red LED configuration:
-static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(LED_NODE_2, gpios);
+static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(LED_NODE_1, gpios);
 
 // TODO: Uncomment this to enable Zephyr logging
 // LOG_MODULE_REGISTER(main, LOG_LEVEL_INF);
@@ -53,11 +52,11 @@ int main(void)
 
         led_state = !led_state;
         TRACE_INF("(%d) LED state: %s", ++count, led_state ? "ON" : "OFF");
-        k_msleep(SLEEP_TIME_MS);
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
 
         // Red LED toggle the GPIO
         if (gpio_pin_toggle_dt(&led_red) < 0) return 0;
-        k_msleep(SLEEP_TIME_MS);
+        k_msleep(CONFIG_APP_HEARTBEAT_PERIOD_MS);
     }
     return 0;
 }
